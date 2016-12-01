d0a48c28b84c97ef971e4f370eaad5cb411cb000
Herb Miller <herb@bobbingwide.com>
2016-12-01 09:37:44 +0000

    Add unit tests for issue #11 and issue #12

---
+++ tests/test-bw_plug.php
@@ -0,0 +1,36 @@
+
+<?php // (C) Copyright Bobbing Wide 2016
+
+/**
+ * @package oik-bob-bing-wide
+ * 
+ * Test the functions in shortcodes/oik-plug.php
+ */
+class Tests_bw_plug extends BW_UnitTestCase {
+
+	function setUp() {
+		parent::setUp();
+		oik_require( "shortcodes/oik-plug.php", "oik-bob-bing-wide" );
+	}
+
+	/**
+	 * Unit test equivalent to [bw_plug oik banner=j]
+	 * 
+	 * The img src is now expected to be http://ps.w.org
+	 */
+	function test_bw_plug_banner_image_url() {
+		$expected_output = '<div class="bw_plug">';
+		$expected_output .= '<a class="bw_banner" href="http://wordpress.org/extend/plugins/oik" title="http://ps.w.org/oik/assets/banner-772x250.jpg">';
+		$expected_output .= '<img class="bw_banner" src="http://ps.w.org/oik/assets/banner-772x250.jpg" title="oik" alt="oik"  />';
+		$expected_output .= '</a>';
+		$expected_output .= '<a class="plugin" href="http://wordpress.org/extend/plugins/oik" title="Link to the oik (oik: Over 80 advanced, powerful shortcodes for displaying the content of your WordPress website.) plugin">oik</a>';
+		$expected_output .= '</div>';
+	
+		$html = bw_plug( array( 'banner' => 'j' ) );
+		bw_trace2( $expected_output, "Expected output" );
+		bw_trace2( $html, "Actual output" );
+		$this->assertEquals( $expected_output, $html );
+		
+	}
+
+}
---
+++ tests/test-github.php
@@ -0,0 +1,32 @@
+<?php // (C) Copyright Bobbing Wide 2016
+
+/**
+ * @package oik-bob-bing-wide
+ * 
+ * Test the functions in shortcodes/oik-github.php
+ */
+class Tests_oik_github extends BW_UnitTestCase {
+
+	function setUp() {
+		parent::setUp();
+		oik_require( "shortcodes/oik-github.php", "oik-bob-bing-wide" );
+	}
+
+	/**
+	 * Unit test equivalent to [github bobbingwide oik-bob-bing-wide assets/oik-bob-bing-wide-banner-772x250.jpg]
+	 */
+	function test_github_type_is_file() {
+		$expected_output = '<a href="https://github.com/bobbingwide/oik-bob-bing-wide"';
+		$expected_output .= ' title="bobbingwide oik-bob-bing-wide">';
+		$expected_output .= '<img class="" src="https://raw.githubusercontent.com/bobbingwide/oik-bob-bing-wide/master/assets/oik-bob-bing-wide-banner-772x250.jpg"';
+		$expected_output .= ' title="bobbingwide oik-bob-bing-wide" alt="bobbingwide oik-bob-bing-wide"  />';
+		$expected_output .= '</a>';
+	
+		$html = bw_github( array( 'bobbingwide', 'oik-bob-bing-wide', 'assets/oik-bob-bing-wide-banner-772x250.jpg' ) );
+		bw_trace2( $expected_output, "Expected output" );
+		bw_trace2( $html, "Actual output" );
+		$this->assertEquals( $expected_output, $html );
+		
+	}
+
+}
