d471192b22b8a9f9a209f9a4002ac2b29b769987
Herb Miller <herb@bobbingwide.com>
2019-01-12 13:18:50 +0000

    Add test for bw_list_dashicons to help code oik-blocks

---
+++ tests/test-bw_list_dashicons.php
@@ -0,0 +1,52 @@
+<?php // (C) Copyright Bobbing Wide 2019
+
+/**
+ * @package oik-bob-bing-wide
+ *
+ * Test the functions in shortcodes/oik-dash.php
+ */
+class Tests_bw_list_dashicons extends BW_UnitTestCase {
+
+	function setUp() {
+		parent::setUp();
+		oik_require( "shortcodes/oik-dash.php", "oik-bob-bing-wide" );
+	}
+
+	/**
+	 * We need a list of dashicons for oik-block
+	 *
+	 * TIL that asort() is very strange indeed!
+	 * $icons[0] may not refer to the first element!
+	 */
+	function test_bw_list_dashicons() {
+		$icons = bw_list_dashicons();
+		//print_r( $icons );
+
+		$this->assertEquals( $icons[0], "menu" );
+		//$this->write_dashicons( $icons );
+		sort( $icons );
+		//print_r( $icons );
+		//$this->write_dashicons( $icons );
+
+		$this->assertEquals( $icons[0], "admin-appearance");
+
+	}
+
+	/**
+	 * This method was used to create oik-blocks\blocks\oik-dashicon\dashiconslist.js
+	 * @param
+	 */
+	function write_dashicons( $icons ) {
+		echo PHP_EOL;
+		echo "const dashicons = [";
+		echo PHP_EOL;
+		foreach ( $icons as $icon ) {
+			echo "'$icon',";
+			echo PHP_EOL;
+		}
+		echo "];";
+		echo PHP_EOL;
+	}
+
+
+}
