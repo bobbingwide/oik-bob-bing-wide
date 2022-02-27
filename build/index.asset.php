1f40c4fefba42a030fc48e120030d9349f66746a
Herb Miller <herb@bobbingwide.com>
2022-02-27 09:09:02 +0000

    Update build using latest wp-scripts v22.0.1

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-server-side-render'), 'version' => '4f47fddc599c0e0c8271e2f9cacec869');
+<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => 'e8596440c9f8fffda6d2fb087c477ce2');
