883c90affd6733af3a1d660843b1f4d9644e0041
Herb Miller <herb@bobbingwide.com>
2022-03-07 12:40:21 +0000

    Update build and languages for v2.2.1 prior to refactoring #46 #42

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => 'e8596440c9f8fffda6d2fb087c477ce2');
+<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-server-side-render'), 'version' => 'e515aa2533fe443554860e7fb90eb4c2');
