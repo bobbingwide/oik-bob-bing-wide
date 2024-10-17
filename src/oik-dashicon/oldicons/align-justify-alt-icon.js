3e0cb7552df04c1755e6ef9d1737e63552d06a6b
Herb Miller <herb@bobbingwide.com>
2024-10-17 09:36:19 +0100

    Add social-link icons copied from Gutenberg and oldicons no longer parts of @wordpress/icons #69

---
+++ src/oik-dashicon/oldicons/align-justify-alt-icon.js
@@ -0,0 +1,14 @@
+/**
+ * WordPress dependencies
+ */
+import { Path, SVG } from '@wordpress/primitives';
+
+export const alignJustifyAlt = () => (
+	<SVG xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" >
+		<Path
+			clipRule="evenodd"
+			fillRule="evenodd"
+			d="M4 5h16v2H4V5zm0 4v2h16V9H4zm0 4h16v2H4v-2zm16 6H4v-2h16v2z"
+		/>
+	</SVG>
+);
---
+++ src/oik-dashicon/oldicons/index.js
@@ -0,0 +1,4 @@
+// WordPress icons which are not / no longer in @wordpress/icons
+// The SVG for these was extracted from a previous version of oik-dash-svg-list.php
+export * from './align-justify-alt-icon';
+export * from './archive-title-icon';
